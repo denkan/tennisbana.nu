5d24b1a6e309fbe7f3e6df92eadbe09856757ba7
Unknown <andreas@stickydirt.se>
2018-04-05 09:53:05 +0200

    Minor tweaks on design

--- www/js/classes/startpage.class.js
+++ www/js/classes/startpage.class.js
@@ -7,11 +7,11 @@ class Startpage extends Base {
 	renderCourt(){
 		let allCourtsHtml = '';
 
-		for (let i = 0; i < app.allCourts.length; i++){
+		for (let court of app.allCourts){
 			allCourtsHtml += `
 			<div class="py-2 px-4 court-item mb-3">
-				<h5 class="mb-0">${app.allCourts[i].title}</h5>
-				<p>Coordinates: ${app.allCourts[i].latitude}, ${app.allCourts[i].longitude}</p>
+				<h5 class="mb-0">${court.title}</h5>
+				<p class="text-muted">${court.address}</p>
 			</div>
 			`
 		}
