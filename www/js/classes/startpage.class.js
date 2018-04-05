bc35b5025ed9dab61e149f0caac21e27a75870f2
Unknown <andreas@stickydirt.se>
2018-04-05 10:16:16 +0200

    Small tweaks

--- www/js/classes/startpage.class.js
+++ www/js/classes/startpage.class.js
@@ -11,11 +11,17 @@ class Startpage extends Base {
 			allCourtsHtml += `
 			<div class="py-2 px-4 court-item mb-3">
 				<h5 class="mb-0">${court.title}</h5>
-				<p class="text-muted">${court.address}</p>
+				<p class="text-muted mb-0">${court.address}</p>
 			</div>
 			`
 		}
 		return allCourtsHtml;
+	}
+
+	click(){
+		if($(event.target).hasClass('cmn-toggle')){
+			console.log('I clicked a checkbox');
+		}
 	}
 
 
